f619343cbf6172b461fef0baf2b5aa184b7c09f6
petrubear <edison.martinez@gmail.com>
2017-09-28 09:51:45 -0500

    update log

--- prepare_log.sh
+++ prepare_log.sh
@@ -5,9 +5,9 @@ timestamp=$(date +"%Y%m%d_%I%M%S")
 req_output_file=${output_dir}'request-'${timestamp}'.txt'
 
 mkdir -p ${output_dir}
-echo 'Input: '$1
-
-echo 'Processing request...'
+echo '[INFO] Input: '$1
+echo ''
+echo '[INFO] Processing request...'
 if [ -f ${req_output_file} ] ; then
     rm ${req_output_file}
 fi
@@ -16,15 +16,15 @@ awk '$0=="---------------------------" {p=1}; p; $0=="--------------------------
 if [ -f ${req_output_file} ] ; then
     sed -i '' 's/--------------------------------------#---------------------------#/\'$'\n/g' ${req_output_file}
     sed -i '' 's/---------------------------#//g' ${req_output_file}
-    echo 'Output: '${req_output_file}
+    echo '[INFO] Output: '${req_output_file}
 else
-   echo 'File not Found: '${req_output_file}
+   echo '[ERROR] File not Found: '${req_output_file}
    exit 1
 fi
 
-echo 'Processing response...'
+echo ''
+echo '[INFO] Processing response...'
 res_output_file=${output_dir}'response-'${timestamp}'.txt'
-echo 'Input: '$1
 
 if [ -f ${res_output_file} ] ; then
     rm ${res_output_file}
@@ -34,8 +34,7 @@ awk '$0=="----------------------------" {p=1}; p; $0=="-------------------------
 if [ -f ${res_output_file} ] ; then
     sed -i '' 's/--------------------------------------#----------------------------#/\'$'\n/g' ${res_output_file}
     sed -i '' 's/----------------------------#//g' ${res_output_file}
-    echo 'Output: '${res_output_file}
+    echo '[INFO] Output: '${res_output_file}
 else
-   echo 'File not Found: '${res_output_file}
+   echo '[ERROR] File not Found: '${res_output_file}
 fi
-
